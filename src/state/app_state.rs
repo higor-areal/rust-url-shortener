6c7da32c4bc74e600411d0c3bf51b63ea2ff691c
Esau Higor <esauhigor@gmail.com>
2026-05-06 15:27:42 -0400

    adicionando derive de Clone no app state

--- src/state/app_state.rs
+++ src/state/app_state.rs
@@ -1,14 +1,9 @@
 use std::collections::HashMap;
 use crate::models::link::Link;
 
+#[derive(Clone)]
 pub struct AppState{
     pub map: HashMap<String, Link>,
     pub end: u32,
-}
-
-impl AppState {
-    fn Add_filed(self: &Self){
-
-    }
 }
 
