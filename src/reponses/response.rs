611a80edc87c3eeaa8a3c98294f30306ffe5a03c
Esau Higor <esauhigor@gmail.com>
2026-05-06 13:58:43 -0400

    Adicionando ResponseErro

---
+++ src/reponses/response.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+pub struct ResponseNewShort{
+    pub status_code: u32,
+    pub short_code: String
+}
+
+#[derive(Serialize)]
+pub struct ResponseGetShorten{
+    pub url: String
+}
+#[derive(Serialize)]
+pub struct ResponseErro{
+    pub status_code: u32,
+    pub message: String,
+}
