1de751304fb0bf61cf2dce3bf772e6323f616d8a
higor-esau <higor.esaut@gmail.com>
2026-06-08 15:31:23 -0400

    path inicial pronto, já usando redis, o restante comitado ainda

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,39 @@ mod reponses;
 
 use axum::{
     Router,
-    routing::{get, post, delete},
+    routing::{get, post},
 };
 
-use std::{
-    collections::HashMap,
-    sync::{
-        Arc,Mutex
-    }
+use deadpool_redis::{
+    Config,
+    Runtime,
 };
+use std::sync::Arc;
 
-use handlers::url_handler::{health, new_shorten, get_shorten, get_links, del_shorten};
+use handlers::url_handler::{health, new_shorten};
 use state::app_state::AppState;
 
 
 #[tokio::main]
 async fn main() {
 
-    let state = AppState{map: HashMap::new(), end: 0};
-    let shared = Arc::new(Mutex::new(state));
-    
+    let cfg = Config::from_url("redis://localhost:6379");
+
+    let redis_pool = cfg
+        .create_pool(Some(Runtime::Tokio1))
+        .expect("Erro ao criar pool Redis");
+
+    let state = Arc::new(AppState {
+        redis: redis_pool.clone(),
+    });
+
     let app = Router::new()
     .route("/", get(health))
     .route("/shorten", post(new_shorten))
-    .route("/r/{code}", get(get_shorten))
-    .route("/links", get(get_links))
-    .route("/links/{code}", delete(del_shorten))
-    .with_state(shared);
+//    .route("/r/{code}", get(get_shorten))
+//    .route("/links", get(get_links))
+//    .route("/links/{code}", delete(del_shorten))
+    .with_state(state);
 
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
