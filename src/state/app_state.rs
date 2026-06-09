fec40c8783935d0a61bf3985b647f01cfe84b618
higor-esau <higor.esaut@gmail.com>
2026-06-08 20:44:46 -0400

    adicionando get_pool()

--- src/state/app_state.rs
+++ src/state/app_state.rs
@@ -1,6 +1,28 @@
-use deadpool_redis::Pool;
+use deadpool_redis::{Connection, Pool};
+
+use crate::reponses::response::Response;
+
+use axum::Json;
 
 #[derive(Clone)]
 pub struct AppState {
     pub redis: Pool,
+}
+
+impl AppState{
+    pub async  fn get_pool(&self) -> Result<Connection, Json<Response>>{
+        match self
+        .redis
+        .get()
+        .await {
+
+        Ok(t) => Ok(t),
+        Err(e) => {
+            Err(Json(Response {
+                status_code: 500,
+                message: format!("erro redis: {:?}", e),
+            }))
+        }
+    }
+    }
 }
