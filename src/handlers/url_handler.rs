08ae11dcb7e2c40af51a1ff45a275045e7b1b295
higor-esau <higor.esaut@gmail.com>
2026-06-10 20:40:06 -0400

    ajustando handlers, estavam ficando muito inflados

--- src/handlers/url_handler.rs
+++ src/handlers/url_handler.rs
@@ -15,12 +15,13 @@ use std::{
     sync::Arc
 };
 
-use rand::Rng;
+
 
 use crate::{
     models::link::{Link, NewLink}, 
     reponses::response::{Response, ResponseGetShorten, ResponseNewShort, ResponseGetLink}, 
-    state::{ app_state::AppState}
+    state::{ app_state::AppState},
+    repositories::redis_repository::RedisStore
 };
 
 
@@ -33,45 +34,29 @@ pub async fn health() -> Json<Value> {
 }
 
 
-pub async fn short_code(size: usize) -> String {
-    let chars = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let mut rng = rand::thread_rng();
 
-    (0..size)
-        .map(|_| {
-            let idx = rng.gen_range(0..chars.len());
-            chars[idx] as char
-        })
-        .collect()
-}
 
 pub async fn new_shorten(
     State(state): State<Arc<AppState>>,
     Json(x): Json<NewLink>
 ) -> Result<Json<ResponseNewShort>, Json<Response>> {
-    print!("{}", x.url);
+
     if x.url.is_empty() {
         return Err(Json(Response{
             status_code: 404,
             message: "Url inválida0".to_string()
         }))
-    }
+    };
 
-    let short = short_code(8).await;
+    let redis = state.redis.clone();
 
-    let mut conn = state.get_pool().await?;
-
-
-    let _: () = conn
-        .hset_multiple(
-            &short,
-            &[("url", x.url.as_str()), ("clicks", "0")]
-        )
-        .await
-        .map_err(|_| Json(Response {
+    let short = match redis.create_link(&x.url).await {
+        Ok(t) => t,
+        Err(msg) => return Err(Json(Response{
             status_code: 404,
-            message: "Url inválida".to_string(),
-        }))?;
+            message: msg
+        }))
+    };
 
     Ok(Json(ResponseNewShort {
         status_code: 201,
@@ -86,49 +71,17 @@ pub async fn get_shorten(
     State(state): State<Arc<AppState>>,
 ) -> Result<Redirect, Json<Response>> {
 
-    let mut conn = state.get_pool().await?;
+    let redis = state.redis.clone();
 
-    let res: HashMap<String, String> = match conn.hgetall(&code).await {
-        Ok(data) => data,
-        Err(_) => {
-            return Err(Json(Response {
-                status_code: 500,
-                message: "Erro ao buscar link".to_string(),
-            }))
-        }
-    };
-
-    if res.is_empty() {
-        return Err(Json(Response {
+    let url = match redis.get_url(&code).await {
+        Ok(t) => t,
+        Err(msg) => return Err(Json(Response{
             status_code: 404,
-            message: "Link não encontrado".to_string(),
-        }));
-    }
-
-    let url = match res.get("url") {
-        Some(url) => url,
-        None => {
-            return Err(Json(Response {
-                status_code: 500,
-                message: "URL não encontrada".to_string(),
-            }))
-        }
+            message: msg
+        }))
     };
 
-    let clicks: i64 = match conn
-        .hincr(&code, "clicks", 1)
-        .await
-    {
-        Ok(value) => value,
-        Err(_) => {
-            return Err(Json(Response {
-                status_code: 500,
-                message: "Erro ao incrementar clicks".to_string(),
-            }))
-        }
-    };
-
-    Ok(Redirect::temporary(url))
+    Ok(Redirect::temporary(&url))
 }
 /* 
 pub async fn get_links(State(state): State<Arc<Mutex<AppState>>>) -> Json<Vec<ResponseGetLink>>{
