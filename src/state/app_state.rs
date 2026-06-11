d00538a415ccf942fc1a8eb623107d7ecca9c35e
higor-esau <higor.esaut@gmail.com>
2026-06-10 20:40:48 -0400

    mudando o state, adicionado redis como estrutura e com comportamento proprio

--- src/state/app_state.rs
+++ src/state/app_state.rs
@@ -1,28 +1,12 @@
 use deadpool_redis::{Connection, Pool};
 
-use crate::reponses::response::Response;
+use crate::{
+    repositories::redis_repository::RedisStore
+};
 
 use axum::Json;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub redis: Pool,
-}
-
-impl AppState{
-    pub async  fn get_pool(&self) -> Result<Connection, Json<Response>>{
-        match self
-        .redis
-        .get()
-        .await {
-
-        Ok(t) => Ok(t),
-        Err(e) => {
-            Err(Json(Response {
-                status_code: 500,
-                message: format!("erro redis: {:?}", e),
-            }))
-        }
-    }
-    }
+    pub redis: RedisStore,
 }
